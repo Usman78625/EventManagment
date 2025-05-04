1327ca067b527f2b25377b094d5f93800bdb7ce4
78625Usman <um7995367@gmail.com>
2025-05-04 21:59:07 +0500

    upl

--- EventManagement.API/Program.cs
+++ EventManagement.API/Program.cs
@@ -111,6 +111,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+  app.UseStaticFiles();
 
 var summaries = new[]
 {
---
+++ EventManagement.Infrastructure/Migrations/20250504162854_AddEventImageUrls.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventImageUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "Users",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrls",
+                table: "Events",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrls",
+                table: "Events");
+        }
+    }
+}
